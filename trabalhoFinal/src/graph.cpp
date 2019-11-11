e8f7bd22dbcbb14a6e729b378cf785e1b66df8f8
RicardoStoklosa <stoklosa.ricardo@gmail.com>
2019-11-11 18:59:55 -0300

    ...

--- trabalhoFinal/src/graph.cpp
+++ trabalhoFinal/src/graph.cpp
@@ -2,6 +2,7 @@
 #include"graph.h"
 
 using namespace std;
+const int INF = 1000000000;
 
 string citiesNames[NUM_CITIES] = {"SP","GOI","FOR","CUR","MAN","BEL","REC","POA","BRA","RJ","VI","SAL","MAC","NA"};
 
@@ -51,42 +52,82 @@ void readInput(vector<Edge> adj[]){
 int hourToMinutes(string h){
     int hour = stoi(h.substr(0,2));
     int minu = stoi(h.substr(2,3));
+    //cout<<hour<<":"<<minu<<"="<<((hour*60)+minu)<<endl;
     return (hour*60)+minu;
 }
 
-int minDistance(int distancia[], bool previous[]){
+int minDistance(int distancia[], bool path[]){
         int mini = INT_MAX;
         int min_index=0;
 
         for(int i=0; i< NUM_CITIES; i++){
-            if(previous[i]==false && distancia[i] <= mini){
+            // cout<<endl<<path[i]<<"-"<<distancia[i]<<mini<<"-"<<endl;
+            if(path[i]==false && distancia[i] <= mini){
                 mini = distancia[i];
                 min_index = i;
             }
         }
-        cout<<"min:"<<min_index<<endl;
+        //cout<<"min:"<<min_index<<endl;
         return min_index;
 }
 
 void dijkstra(int start, vector< Edge > adj[]){
     int dist[NUM_CITIES];
-    bool previous[NUM_CITIES];
+    int path[NUM_CITIES];
+    int time[NUM_CITIES];
+    bool mark[NUM_CITIES];
 
-    memset(dist,INT_MAX,sizeof(dist));
-    memset(previous,true,sizeof(previous));
+    for(int i=0; i<NUM_CITIES; i++){
+        dist[i] = INF;
+    }
+    memset(path,-1,sizeof(path));
+    memset(time,-1,sizeof(path));
+    memset(mark,false,sizeof(mark));
 
     dist[start] = 0;
-
-    for(int i=0; i<NUM_CITIES-1;i++){
-        int u = minDistance(dist,previous);
-        previous[u]=true;
+    string city = "";
+    for(int i=0; i<NUM_CITIES; i++){
+        int v = -1;
         for(int j=0; j<NUM_CITIES; j++){
-            for(Edge edge:adj[u]){
-                if(previous[j]==0 && edge.node==j && dist[u] != INT_MAX && (dist[u] + edge.cost) < dist[j])
-                    dist[j] =  dist[u] + edge.cost;
+            if(!mark[j] && (v==-1 || dist[j]<dist[v]))
+                v=j;
+        }
+
+        if(dist[v]==INF)
+            break;
+
+        mark[v]=true;
+        //cout<<city<<"=>"<<citiesNames[v]<<"["<<time<<"]"<<endl;
+        cout<<"{"<<endl;
+        for(Edge edge : adj[v]){
+            cout<<"{"<<citiesNames[v]<<"->"<<citiesNames[edge.node]<<" "<<time[v]<<"}"<<endl;
+
+            int to = edge.node;
+            int cost = edge.cost;
+
+            if((dist[v]+cost)<dist[to]){
+                dist[to]=dist[v]+cost;
+                path[to]=v;
+                time[v]=edge.arrival;
+                // cout<<citiesNames[v]<<"=>"<<citiesNames[to]<<"["<<time<<"]"<<endl;
+                city=citiesNames[v];
             }
         }
+        cout<<"}"<<endl;
     }
-    //cout<<endl<<dist[getCity("NA")]<<endl;;
+
+    vector<int> pathe;
+    int t=getCity("NA");
+    for (int v = t; v != getCity("SP"); v = path[v])
+        pathe.push_back(v);
+    pathe.push_back(getCity("SP"));
+
+    reverse(pathe.begin(), pathe.end());
+    for(int p:pathe){
+        if(p!=-1)
+            cout<<citiesNames[p]<<endl;
+    }
+    cout<<"Distancia "<<dist[getCity("NA")]<<endl;
+
 }
 
--- trabalhoFinal/src/main.cpp
+++ trabalhoFinal/src/main.cpp
@@ -8,6 +8,6 @@ int main(){
     readInput(adj);
     //addEdge(adj,getCity("SP"),getCity("POA"),2,4,hourToMinutes("1000"),hourToMinutes("1100"));
     //addEdge(adj,getCity("SP"),getCity("MAN"),4,8,hourToMinutes("1100"),hourToMinutes("1920"));
-    showAdjList(adj);
+    //showAdjList(adj);
     dijkstra(getCity("SP"),adj);
 }
